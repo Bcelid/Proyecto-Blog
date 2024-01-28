2fa49f0e70f55e66fc5e5dbf7ccbdd74c9498169
Bcelid <58522868+Bcelid@users.noreply.github.com>
2024-01-27 19:30:07 -0500

    lastVersion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -22,6 +23,22 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        // Insertamos dos registros en la tabla users durante la migración
+        DB::table('users')->insert([
+            'name' => 'Byron Celi',
+            'email' => 'byron@gmail.com',
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Carlos Diaz',
+            'email' => 'carlos@gmail.com',
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
@@ -31,6 +48,13 @@ return new class extends Migration
      */
     public function down()
     {
+        // Desactiva temporalmente las restricciones de clave externa
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        // Elimina la tabla
         Schema::dropIfExists('users');
+
+        // Activa las restricciones de clave externa nuevamente
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 };
